052c3186866e13e21a3d99e499eedf38235e2878
SoumyaSubhamPatra <soumya0299@gmail.com>
2024-09-02 13:07:10 +0530

    Login changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AuthProvider } from './context/AuthContext';
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+// import './index.css'
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <AuthProvider>
-  <App />
-</AuthProvider>,
-)
+    <App />
+  </AuthProvider>
+);
